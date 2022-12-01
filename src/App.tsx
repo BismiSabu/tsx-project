9498fe94b34ad44a94de007f9b40b80543aaf0ee
BISMI  SABU <117430250+BismiSabu@users.noreply.github.com>
2022-12-01 11:18:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       
+        <p>git fetch concept</p>
         <a
           className="App-link"
           href="https://reactjs.org"
